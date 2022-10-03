3c68e3e3a98707a97c6f64660f6325a79f083134
I-zme <inbar.amir21@gmail.com>
2022-10-03 16:04:37 +0700

    Fix keyboard navigation

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-
 const calculatorDisplay = document.querySelector('.display');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const modulators = document.querySelectorAll('.mod');
 
 
-const buttons = document.querySelectorAll('button');
+
+
 
 function moveBy(number){
     let tempLocationIndex = document.activeElement.getAttribute("data-index-number");
     let newLocationIndex = Number(tempLocationIndex)+number;
+    if(newLocationIndex>20){
+        newLocationIndex = newLocationIndex-20;
+    }
+    if(newLocationIndex<1){
+        newLocationIndex = 20+newLocationIndex;
+    }
     document.querySelector(`[data-index-number='${newLocationIndex}']`).focus();
 }   
 
@@ -23,12 +29,13 @@ document.addEventListener('keydown',e=>{
             moveBy(1);
             break
         case 'ArrowUp':
+            moveBy(-4)
             break
         case 'ArrowDown':
+            moveBy(4)
             break
         case 'Enter':
             break
-
     }
 });
 
